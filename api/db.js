426445b00786e55ad79e8db3eabb65447127eea6
rbison84 <masood.ross@gmail.com>
2025-01-19 21:17:55 -0500

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
 const client = new MongoClient(process.env.MONGODB_URI);
 
+console.log(process.env.MONGODB_URI); // Logs the URI to verify it's working
+
 module.exports = client;
